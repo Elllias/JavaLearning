a1aa3164a1ee43000994b9c06fc9a509ed419702
Волков Илья <ellias_vo312@mail.ru>
2022-03-09 15:16:01 +0500

    Залил решение первого домашнего задания

---
+++ src/com/company/Main.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        MyString myStr = new MyString("UrFU norm Unik");
+        myStr.PrintString();
+    }
+}
---
+++ src/com/company/MyString.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class MyString {
+    private String string;
+
+    MyString(String str){
+        string = str;
+    }
+
+    public void PrintString(){
+        System.out.print(string);
+    }
+}
